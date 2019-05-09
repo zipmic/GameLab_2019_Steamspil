6ad16ed851c8c86e98b8015209290d628212bcd4
Code <mail@code.dk>
2019-05-09 10:50:16 +0200

    Tilføjet en slutning

--- Assets/Code/Scripts/Code_EndBikiniQuest.cs
+++ Assets/Code/Scripts/Code_EndBikiniQuest.cs
@@ -7,6 +7,7 @@ public class Code_EndBikiniQuest : MonoBehaviour
 
     public GameObject Player;
     public GameObject BikiniGirl;
+    public GameObject CanvasBlack;
     
     // Start is called before the first frame update
     void Start()
@@ -22,8 +23,16 @@ public class Code_EndBikiniQuest : MonoBehaviour
         if(other.tag == "Player" && once == false)
         {
             once = true;
-            Player.GetComponent<Collider>().enabled = false;
-
+            Player.GetComponent<Collider2D>().enabled = false;
+            Player.GetComponent<PlayerMovement>().enabled = false;
+            BikiniGirl.GetComponent<Animator>().Play("EekAnimation");
+            Player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            Invoke("CanvasOn", 1.2f);
         }
+    }
+
+    void CanvasOn()
+    {
+        CanvasBlack.gameObject.SetActive(true);
     }
 }
